b2d45f6f52dd2800e0968a153d0610c37ea20572
Joaquin Perez <joaquin.perez.coria@gmail.com>
2023-08-22 14:31:35 -0300

    fix(logger.test.router): Ahora el endpoint para acceder al testing del logger es solo /loggerTest

--- src/routes/logger.test.routes.js
+++ src/routes/logger.test.routes.js
@@ -3,4 +3,4 @@ import { loggerTestController } from "../controllers/logger.test.controller.js";
 
 export const loggerTest = express.Router();
 
-loggerTest.get("/loggerTest", loggerTestController.loggerTest);
+loggerTest.get("/", loggerTestController.loggerTest);
